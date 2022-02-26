45a7342d6e6b424fbab1ebcab69e8df2cd2d9798
ketone <adi.asyraaf@gmail.com>
2022-02-26 12:17:25 +0800

    Update ledblink.c

--- ledblink.c
+++ ledblink.c
@@ -7,16 +7,24 @@
 # define F_CPU 16000000UL
 #endif
 
+#define __DELAY_BACKWARD_COMPATIBLE__
+
 #include <avr/io.h>
 #include <util/delay.h>
+
+
+
 int main(void)
 {
-    DDRC |= 1 << PINC5; //define the pin as output
+   
+
+    DDRD |= 1 << PIND7; //define the pin as output
     while (1)
     {
         _delay_ms(1000);     //execute delay 1s
  
-        PORTC ^= 1 << PINC5; // toggle the outout pin
+        PORTD ^= 1 << PIND7; // toggle the outout pin
+
     }
     return 0;
 }
